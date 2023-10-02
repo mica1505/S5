30f880002181fc65378d8041f710929866b0262f
mica1505 <cameliamazouz1505@gmail.com>
2023-10-02 23:38:27 +0200

    Finito TP liste chainees

--- Algo/TP1.c
+++ Algo/TP1.c
@@ -30,13 +30,33 @@ void insererCel(cel *liste,int pos, cel *cellule){
     cel *posListe = liste;
     int k;
     for(k=1;k<pos;k++,posListe=posListe->succ);
-    printf("-- k : %d -- \n", k);
     cellule->succ = posListe->succ;
     cellule->pre = posListe;
     if(posListe->succ!=NULL){
         (cellule->succ)->pre = cellule;
     }
     posListe->succ=cellule;  
+}
+
+void suppCel(int pos, cel * liste){
+    //on peut pas supprimer le premier element de la liste, on le suprime on perd le debut de la liste.
+    cel *posListe = liste;
+    int i;
+    if(pos == 1){
+        printf("On ne peut pas supprimer le premier element de la liste.");
+    }
+    else{
+
+        for(i=1;i<pos;i++,posListe = posListe->succ);
+        if(posListe->succ==NULL){
+            posListe->pre->succ = NULL;
+        }
+        else{
+            posListe->succ->pre = posListe->pre;
+            posListe->pre->succ = posListe->succ;
+        }
+        free(posListe);
+    }
 }
 
 void afficher(cel * liste){
@@ -48,23 +68,62 @@ void afficher(cel * liste){
 }
 
 int main(){
-    point p,p2;
+    //menu 
+    char choix;
+    int quitter= 0;
+    cel * poly;
+    printf("insertion de point\n");
+    point p1;
+    scanf("Entrez x : %d", &p1.x);
+    scanf("Entrez y : %d", &p1.y);
+    poly = nouvCell(p1);
+    while(!quitter){
+        printf("Que voulez vous faire? (i->inserer, s->supprimer, q->quitter)");
+        scanf("%s",&choix);
+        switch (choix) {
+            case 'i':
+                printf("insertion de point\n");
+                point p;
+                scanf("Entrez x : %d", &p.x);
+                scanf("Entrez y : %d", &p.y);
+                afficher(poly);
+                break;
+            case 's':
+                printf("suppresison de point\n");
+                int ind;
+                scanf("Entrez l'indice du point que vous voulez supprimer : %d", &ind);
+                suppCel(ind,poly);
+                afficher(poly);
+                break;
+            case 'q':
+                afficher(poly);
+                printf("QUITTER\n");
+                free(poly);
+                quitter = 1;
+        }
+    }
+ /*  point p,p2,p3;
     p.x = 1;
     p.y = 2; 
 
     cel * ajoutCel = nouvCell(p);
-    afficher(ajoutCel);
-    printf("//\n");
+
     p2.x = 4;
     p2.y = 5;
     cel * nvpoint = nouvCell(p2);
-    afficher(nvpoint);
-    printf("//\n");
+
+    p3.x = 6;
+    p3.y = 7;
+    cel * point3 = nouvCell(p3);
+
     insererCel(ajoutCel,1,nvpoint);
-    //ajoutCel->succ = nvpoint;
+    insererCel(ajoutCel,2,point3);
     afficher(ajoutCel);
 
+    suppCel(1,ajoutCel);
+
+    afficher(ajoutCel);
     free(ajoutCel);
-    free(nvpoint);
+    free(nvpoint);*/
     return EXIT_SUCCESS;
 }
